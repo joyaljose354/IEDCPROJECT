d7a18444142b379ab03cef42fd30e1aa86eff1fc
joyaljose354 <joyaljose354@gmail.com>
2016-09-13 21:53:06 +0800

    Add files via upload
    by joyal

---
+++ collision.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collision : MonoBehaviour 
+{
+	public GUIText score;
+	public int count;
+	public GameObject ball;
+	public GameObject ball2;
+	public GameObject ball3;
+	public GameObject ball4;
+
+	void start()
+	{
+		count = 0;
+
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("purple"))
+			{
+				ball.SetActive (false);
+				count = count + 1;
+			OnGUI();
+			}
+		if(other.gameObject.CompareTag("yellow"))
+		{
+			ball2.SetActive (false);
+			count = count + 1;
+			OnGUI();
+		}
+		if(other.gameObject.CompareTag("pink"))
+		{
+			ball3.SetActive (false);
+			count = count + 1;
+			OnGUI ();
+		}
+		if(other.gameObject.CompareTag("teal"))
+		{
+			ball4.SetActive (false);
+			count = count + 1;
+			OnGUI ();		}
+	}
+	void OnGUI ()
+	{
+		GUI.Label (new Rect (5, 5, 200, 30), "score" + count);
+}
+}
+
+
+
+
---
+++ spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+	
+
+	public Transform ballPos;
+public Rigidbody Ball;
+
+public float timeMax = 1;
+
+	private int count;
+	float rand;
+
+void SpawnBall()
+	{	
+		float rb =Random.Range (-4f, 4f);
+		Vector3 postn = new Vector3 (rb, 8f,0f);
+	Rigidbody ballRigid;
+		ballRigid = Instantiate(Ball,postn,Quaternion.identity) as Rigidbody;
+
+}
+void Start() 
+	{
+	float timeToSpawn = timeMax;
+		timeMax++;
+	InvokeRepeating("SpawnBall", timeToSpawn,4f);
+	}
+		
+
+}
